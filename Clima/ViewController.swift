6c8368c7e4fa64aee15c38241135ccc0a6a19cb9
Fantomas <1234556789>
2021-04-07 11:30:49 +0300

    Initial Commit

---
+++ Clima/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Onopriienko.Sergii on 07.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
